0be72c537e7a62b7d9455af663bca088c88dfdaa
ArthurBauu <arthurbaudouin05@gmail.com>
2026-01-18 15:13:43 +0100

    Clean and subject

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -13,21 +13,21 @@ int main () {
 	int array[] = {1, 2, 3, 4, 5};
 	size_t length = sizeof(array) / sizeof(array[0]);
 
-	std::cout << "Testing iter with int array:" << std::endl;
+	std::cout << "\nTesting iter with int array:" << std::endl;
 	std::cout << "Original array: ";
 	iter(array, length, print);
 	std::cout << std::endl;
 
 
-	std::cout << "Testing iter with char array to convert to uppercase:" << std::endl;
+	std::cout << "\nTesting iter with char array to convert to uppercase:" << std::endl;
 	std::string message = "Hello, World!";
 	size_t msg_length = message.size();
-
 	std::cout << "Original message: " << message << std::endl;
 	iter(&message[0], msg_length, to_upper);
+	std::cout << "Modified message: " << message << std::endl;
 
 
-	std::cout << "Testing iter with const int array:" << std::endl;
+	std::cout << "\nTesting iter with const int array:" << std::endl;
 	const int const_array[] = {10, 20, 30};
 	size_t const_length = sizeof(const_array) / sizeof(const_array[0]);
 	std::cout << "Const array: ";
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,20 +2,37 @@
 
 
 int main() {
+
+    //creating Array of int of size 5
+    std::cout << "Creating Array of int of size 5" << std::endl;
     Array<int> arr(5);
     arr[0] = 10;
-    std::cout << arr[0] << std::endl;
-    std::cout << arr.getSize() << std::endl;
+    std::cout << "arr[0]: " << arr[0] << std::endl;
+    std::cout << "Size: " << arr.getSize() << std::endl;
 
+    //testing out of bounds
+    std::cout << "\nTesting out of bounds" << std::endl;
     try {
         arr[10] = 1;
     } catch (std::exception &e) {
         std::cout << "Exception: " << e.what() << std::endl;
     }
 
+    // Testing other things
+    // std::cout << "\nTesting out of bounds" << std::endl;
+    // try {
+    //     arr[4] = 1;
+    //     std::cout << arr[4] << std::endl;
+    // } catch (std::exception &e) {
+    //     std::cout << "Exception: " << e.what() << std::endl;
+    // }
+
+    // Testing copy constructor
+    std::cout << "\nTesting copy constructor" << std::endl;
     Array<int> copy = arr;
-	std::cout << copy[0] << std::endl;
+	std::cout << "copy[0]: " << copy[0] << std::endl;
+    std::cout << "Modifying copy[0] to 20" << std::endl;
     copy[0] = 20;
-    std::cout << arr[0] << " " << copy[0] << std::endl;
+    std::cout << "arr[0]: " << arr[0] << " copy[0]: " << copy[0] << std::endl;
     return 0;
 }
